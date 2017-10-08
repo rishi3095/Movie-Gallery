ea27371b6e58395eff65623ccb125031187bed39
rishi3095 <rishi3095@github.com>
2017-10-08 17:56:46 +0530

    database name changed

--- db_connection.php
+++ db_connection.php
@@ -5,7 +5,7 @@ function OpenConnection()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "MovieDB"; 
+ $db = "MovieGallery"; 
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  return $conn;
  }
